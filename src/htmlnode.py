374b1e910c7b87eb08901cb22ccb7981a90e5914
stousignant <sacha.tousignant@gmail.com>
2026-01-14 08:37:35 -0500

    Text node and HTML node with tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag;
+        self.value = value;
+        self.children = children;
+        self.props = props;
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        props_str = " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        return " " + props_str
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
